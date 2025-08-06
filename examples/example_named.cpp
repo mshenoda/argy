85f4b2c01053a59a4df6aa39016abe16a8363a03
mshenoda <michael@mshenoda.com>
2025-08-06 15:54:01 -0400

    Updated examples

--- examples/example_named.cpp
+++ examples/example_named.cpp
@@ -11,10 +11,11 @@ int main(int argc, char* argv[]) {
     Argy::Parser args(argc, argv);
 
     // Named convenience methods for argument definitions
-    args.addString({"-o", "--output"}, "Output directory", "results/");
+    args.addString("file", "Input File");
+    args.addString({"-o", "--output"}, "Output directory");
     args.addFloat({"-nms", "--nms-thresh"}, "NMS threshold", 0.45f);
     args.addBool({"-v", "--verbose"}, "Enable verbose output", false);
-    args.addInts({"-s", "--input-shape"}, "Input shape (HxW)", Ints{640, 480});
+    args.addInts({"-s", "--input-shape"}, "Input shape (HxW)", vector<int>{640, 480});
 
     try {
         args.parse();
