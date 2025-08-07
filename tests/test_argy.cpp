9b2d042eb9b9f6728d9baa20c85a935def6ccc5e
mshenoda <michael@mshenoda.com>
2025-08-07 11:31:28 -0400

    Updated tests

--- tests/test_argy.cpp
+++ tests/test_argy.cpp
@@ -135,3 +135,143 @@ TEST_CASE("Type validation") {
     parser.addInt("number", "A number");
     CHECK_THROWS_AS(parser.parse(), std::invalid_argument);
 }
+
+TEST_CASE("Long name only arguments for all types") {
+    // String
+    {
+        const char* argv[] = {"prog", "--filename", "input.txt"};
+        int argc = 3;
+        Parser parser(argc, const_cast<char**>(argv));
+        parser.addString("--filename", "Input file");
+        parser.parse();
+        CHECK(parser.getString("filename") == "input.txt");
+    }
+    // Int
+    {
+        const char* argv[] = {"prog", "--count", "42"};
+        int argc = 3;
+        Parser parser(argc, const_cast<char**>(argv));
+        parser.addInt("--count", "Count");
+        parser.parse();
+        CHECK(parser.getInt("count") == 42);
+    }
+    // Bool
+    {
+        const char* argv[] = {"prog", "--flag"};
+        int argc = 2;
+        Parser parser(argc, const_cast<char**>(argv));
+        parser.addBool("--flag", "A flag", false);
+        parser.parse();
+        CHECK(parser.getBool("flag") == true);
+    }
+    // Vector (of strings)
+    {
+        const char* argv[] = {"prog", "--names", "Alice", "Bob", "Charlie"};
+        int argc = 5;
+        Parser parser(argc, const_cast<char**>(argv));
+        parser.addStrings("--names", "List of names");
+        parser.parse();
+        auto names = parser.getStrings("names");
+        CHECK(names.size() == 3);
+        CHECK(names[0] == "Alice");
+        CHECK(names[1] == "Bob");
+        CHECK(names[2] == "Charlie");
+    }
+    // Vector (of ints)
+    {
+        const char* argv[] = {"prog", "--numbers", "1", "2", "3", "4"};
+        int argc = 6;
+        Parser parser(argc, const_cast<char**>(argv));
+        parser.addInts("--numbers", "List of numbers");
+        parser.parse();
+        auto numbers = parser.getInts("numbers");
+        CHECK(numbers.size() == 4);
+        CHECK(numbers[0] == 1);
+        CHECK(numbers[1] == 2);
+        CHECK(numbers[2] == 3);
+        CHECK(numbers[3] == 4);
+    }
+    // Vector (of floats)
+    {
+        const char* argv[] = {"prog", "--values", "1.1", "2.2", "3.3"};
+        int argc = 5;
+        Parser parser(argc, const_cast<char**>(argv));
+        parser.addFloats("--values", "List of floats");
+        parser.parse();
+        auto values = parser.getFloats("values");
+        CHECK(values.size() == 3);
+        CHECK(values[0] == doctest::Approx(1.1));
+        CHECK(values[1] == doctest::Approx(2.2));
+        CHECK(values[2] == doctest::Approx(3.3));
+    }
+}
+
+TEST_CASE("Long name only arguments with default values and override") {
+    // String
+    {
+        const char* argv[] = {"prog", "--filename", "input.txt"};
+        int argc = 3;
+        Parser parser(argc, const_cast<char**>(argv));
+        parser.addString("--filename", "Input file", "default.txt");
+        parser.parse();
+        CHECK(parser.getString("filename") == "input.txt");
+    }
+    // Int
+    {
+        const char* argv[] = {"prog", "--count", "42"};
+        int argc = 3;
+        Parser parser(argc, const_cast<char**>(argv));
+        parser.addInt("--count", "Count", 99);
+        parser.parse();
+        CHECK(parser.getInt("count") == 42);
+    }
+    // Bool
+    {
+        const char* argv[] = {"prog", "--flag"};
+        int argc = 2;
+        Parser parser(argc, const_cast<char**>(argv));
+        parser.addBool("--flag", "A flag", false);
+        parser.parse();
+        CHECK(parser.getBool("flag") == true);
+    }
+    // Vector (of strings)
+    {
+        const char* argv[] = {"prog", "--names", "Alice", "Bob", "Charlie"};
+        int argc = 5;
+        Parser parser(argc, const_cast<char**>(argv));
+        parser.addStrings("--names", "List of names", std::vector<std::string>{"Default"});
+        parser.parse();
+        auto names = parser.getStrings("names");
+        CHECK(names.size() == 3);
+        CHECK(names[0] == "Alice");
+        CHECK(names[1] == "Bob");
+        CHECK(names[2] == "Charlie");
+    }
+    // Vector (of ints)
+    {
+        const char* argv[] = {"prog", "--numbers", "1", "2", "3", "4"};
+        int argc = 6;
+        Parser parser(argc, const_cast<char**>(argv));
+        parser.addInts("--numbers", "List of numbers", std::vector<int>{99, 100});
+        parser.parse();
+        auto numbers = parser.getInts("numbers");
+        CHECK(numbers.size() == 4);
+        CHECK(numbers[0] == 1);
+        CHECK(numbers[1] == 2);
+        CHECK(numbers[2] == 3);
+        CHECK(numbers[3] == 4);
+    }
+    // Vector (of floats)
+    {
+        const char* argv[] = {"prog", "--values", "1.1", "2.2", "3.3"};
+        int argc = 5;
+        Parser parser(argc, const_cast<char**>(argv));
+        parser.addFloats("--values", "List of floats", std::vector<float>{9.9f, 8.8f});
+        parser.parse();
+        auto values = parser.getFloats("values");
+        CHECK(values.size() == 3);
+        CHECK(values[0] == doctest::Approx(1.1));
+        CHECK(values[1] == doctest::Approx(2.2));
+        CHECK(values[2] == doctest::Approx(3.3));
+    }
+}
